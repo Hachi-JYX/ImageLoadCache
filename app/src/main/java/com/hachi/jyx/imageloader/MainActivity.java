5dc9ecc717f27ab7443d99fb82de73b2006b08b7
Hachi-JYX <15221613915@163.com>
2016-05-25 18:05:24 +0800

    first commint

--- app/src/main/java/com/hachi/jyx/imageloader/MainActivity.java
+++ app/src/main/java/com/hachi/jyx/imageloader/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageView imageView = (ImageView) findViewById(R.id.iv);
+        //呵呵
         String url = "http://www.k618.cn/ygmp/dzrmp/zrfg/201205/W020120523404414407920.jpg";
         ImageLoadCache imageLoadCache = new ImageLoadCache(getBaseContext());
         imageLoadCache.displayImage(imageView,url);
